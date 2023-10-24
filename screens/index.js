15e3076ea9a9a31f8d7f34972aba60bfb918adb8
rajanaufalz <rajanaufalz1273@gmail.com>
2023-10-24 13:48:39 +0700

    modul04

---
+++ screens/index.js
@@ -0,0 +1,29 @@
+import LotsOfStyles from "./LotsOfStyles";
+import FixedDimensionBasics from "./FixedDimensionBasics";
+import FlexDimensionBasics from "./FlexDimensionBasics";
+import PercentageDimensionBasics from "./PercentageDimensionBasics";
+import FlexBasic from "./FlexBasic";
+import FlexDirectionBasics from "./FlexDirectionBasics";
+import JustifyContentBasics from "./JustifyContentBasics";
+import AlignItemsLayout from "./AlignItemsLayout";
+import WidthHeightBasics from "./WidthHeightBasics";
+import PositionLayout from "./PositionLayout";
+import DisplayAnImageWithStyle from "./DisplayAnImageWithStyle";
+import NativeBaseBasic from "./NativeBaseBasic";
+import GlueStackUIBasic from "./GlueStackUIBasic";
+
+export {
+  LotsOfStyles,
+  FixedDimensionBasics,
+  FlexDimensionBasics,
+  PercentageDimensionBasics,
+  FlexBasic,
+  FlexDirectionBasics,
+  JustifyContentBasics,
+  AlignItemsLayout,
+  WidthHeightBasics,
+  PositionLayout,
+  DisplayAnImageWithStyle,
+  NativeBaseBasic,
+  GlueStackUIBasic,
+};
